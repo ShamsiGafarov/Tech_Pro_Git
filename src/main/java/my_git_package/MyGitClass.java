637492d66e121f7bcf2cc56b38e2e63a0e7294af
ShamsiGafarov <shamsigafarov@gmail.com>
2023-03-13 16:08:45 -0400

    Initial commit

---
+++ src/main/java/my_git_package/MyGitClass.java
@@ -0,0 +1,13 @@
+package my_git_package;
+
+public class MyGitClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Version 1");
+
+
+
+
+    }
+}
---
+++ src/main/java/my_git_package/MySecondClass.java
@@ -0,0 +1,10 @@
+package my_git_package;
+
+public class MySecondClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my second class");
+
+    }
+}
